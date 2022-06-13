ea57a4a1abac3c347301ac723224cdae881dbe93
Muhammad Arizky <arizkymuhammad46@gmail.com>
2022-06-13 15:25:36 +0700

    Added new features

--- js/script.js
+++ js/script.js
@@ -35,6 +35,52 @@ function generateTodoObject(id, task, timestamp, isCompleted) {
     }
 }
 
+function findTodo(todoId) {
+    for (const todoItem of todos) {
+        if (todoItem.id === todoId) {
+            return todoItem;
+        }
+    }
+    return null;
+}
+
+function findTodoIndex(todoId) {
+    for (const index in todos) {
+        if (todos[index].id === todoId) {
+            return index;
+        }
+    }
+
+    return -1;
+}
+
+function addTaskToCompleted(todoId) {
+    const todoTarget = findTodo(todoId);
+
+    if (todoTarget == null) return;
+
+    todoTarget.isCompleted = true;
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function removeTaskFromCompleted(todoId) {
+    const todoTarget = findTodoIndex(todoId);
+
+    if (todoTarget === -1) return;
+
+    todos.splice(todoTarget, 1);
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function undoTaskFromCompleted(todoId) {
+    const todoTarget = findTodo(todoId);
+
+    if (todoTarget == null) return;
+
+    todoTarget.isCompleted = false;
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
 function makeTodo(todoObject) {
     const textTitle = document.createElement('h2');
     textTitle.innerText = todoObject.task;
@@ -51,6 +97,32 @@ function makeTodo(todoObject) {
     container.append(textContainer);
     container.setAttribute('id', `todo-${todoObject.id}`);
 
+    if (todoObject.isCompleted) {
+        const undoButton = document.createElement('button');
+        undoButton.classList.add('undo-button');
+
+        undoButton.addEventListener('click', function () {
+            undoTaskFromCompleted(todoObject.id);
+        });
+        const trashButton = document.createElement('button');
+        trashButton.classList.add('trash-button');
+    
+        trashButton.addEventListener('click', function () {
+            removeTaskFromCompleted(todoObject.id);
+        });
+
+        container.append(undoButton, trashButton);
+    } else {
+        const checkButton = document.createElement('button');
+        checkButton.classList.add('check-button');
+
+        checkButton.addEventListener('click', function () {
+            addTaskToCompleted(todoObject.id);
+        });
+
+        container.append(checkButton);
+    }
+
     return container;
 }
 
@@ -59,8 +131,15 @@ document.addEventListener(RENDER_EVENT, function () {
     const uncompletedTODOList = document.getElementById('todos');
     uncompletedTODOList.innerHTML = '';
 
+    const completedTODOList = document.getElementById('completed-todos');
+    // completedTODOList.innerHTML = '';
+
     for (const todoItem of todos) {
         const todoElement = makeTodo(todoItem);
-        uncompletedTODOList.append(todoElement);
+        if (!todoItem.isCompleted) {
+            uncompletedTODOList.append(todoElement);
+        } else {
+            completedTODOList.append(todoElement);
+        }
     }
 });
